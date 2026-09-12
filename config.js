aaa1acb09ffe1cdad94bf444acda883f9919b6ca
isea-III <isea@mnnit.ac.in>
2026-09-12 22:26:44 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+// Central configuration file for ISEA Phase-III & MNNIT programs.
+// To add a new program in the future, simply add a new key-value pair below.
+const PORTAL_CONFIG = {
+    "ngcsp_2026": {
+        title: "Next-Generation Cyber Security and Privacy (NGCSP-2026) - ISEA III",
+        apiUrl: "YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE"
+    }
+    
+    /* 
+    // Example for future programs:
+    "future_event_2026": {
+        title: "Future Workshop Name (2026)",
+        apiUrl: "ANOTHER_GOOGLE_APPS_SCRIPT_WEB_APP_URL"
+    }
+    */
+};
