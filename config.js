d15543921e70d9fd200b701645deec1bc05bb37d
isea-III <isea@mnnit.ac.in>
2026-09-13 21:32:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,33 +6,10 @@ const PORTAL_CONFIG = {
         apiUrl: "https://script.google.com/macros/s/AKfycbyFHnN4VLG7-nhtJhHBqN6-mFga0oG0uAuyy2LPYdkMzo-Hnd9GyTqFZpu9QyDzoOe5/exec" 
     },
     "sample_event": {
-        title: "Sample Verification Program (SVP 2026)",
-        logo: "https://lh3.googleusercontent.com/d/1t701aaqGVYeFNbf2qrF_U0f-S1OqgARd", // Replace with sample logo
+        title: "Joint Hackthon 2026 NIT Allahabad & NIT NIT Kurukshetra",
+        logo: "https://lh3.googleusercontent.com/d/1A5tXcasHlKlrwE9jI3DJlVWGv3FZ71FG", // Replace with sample logo
         color: "#8B4513",
         apiUrl: "YOUR_SAMPLE_APPS_SCRIPT_URL_HERE" 
-    },
-    "mhwb": {
-        title: "Mental Health & Well-Being (MHWB 2026)",
-        logo: "https://lh3.googleusercontent.com/d/1Gb3BDjHimwaItoYwc5Z4aCVxQCUBVAHS",
-        color: "#2E7D32",
-        apiUrl: "YOUR_MHWB_APPS_SCRIPT_URL_HERE" 
-    },
-    "pewp": {
-        title: "Pedagogical Excellence & Wellness Program (PEWP 2026)",
-        logo: "https://lh3.googleusercontent.com/d/1txoD86Nb06-_fiA0Kwf_nc7WZifo0rUg",
-        color: "#1565C0",
-        apiUrl: "https://script.google.com/macros/s/AKfycbwsOY1OUDXxhGqtT4DLoMaEn7ONMq_Y1kWtsVa1m2IDebtfb5NDRUNzD38QmyJjimh3/exec" 
-    },
-    "reccb": {
-        title: "Research Excellence & Campus Culture Building (RECCB 2026)",
-        logo: "https://lh3.googleusercontent.com/d/1t701aaqGVYeFNbf2qrF_U0f-S1OqgARd",
-        color: "#606060",
-        apiUrl: "YOUR_RECCB_APPS_SCRIPT_URL_HERE" 
-    },
-    "smsw": {
-        title: "Student Mentoring Skill Workshop (SMSW 2026)",
-        logo: "https://lh3.googleusercontent.com/d/1K40v_xuCoqHWpjlKw4PEH0gUB94FitbE",
-        color: "#855fec",
-        apiUrl: "YOUR_SMSW_APPS_SCRIPT_URL_HERE" 
     }
+   
 };
